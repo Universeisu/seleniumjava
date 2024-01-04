796a609b81cc9d48a0817622a8958f7441547ec9
Universeisu <111554684+Universeisu@users.noreply.github.com>
2024-01-04 14:41:22 +0700

    Add files via upload

---
+++ selenium.js/src/selenium/js/TestDriver.java
@@ -0,0 +1,15 @@
+package selenium.js;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+        WebDriver driver = null;
+        System.setProperty("webdriver.chrome.driver", "D:\\webdriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://sc.npru.ac.th");
+        driver.quit();
+	}
+
+}
